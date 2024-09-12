246864c592f3ca638769dbfcef0deae709c0b981
npdao1992 <npdao1992@gmail.com>
2024-09-12 23:29:42 +0700

    Update: - Search in Home - Datatable js (2/10) Product and Category -  Demo (TEST) - Price VNĐ - Show related product details - Product details

--- EShop/Controllers/ProductController.cs
+++ EShop/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using EShop.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EShop.Controllers
 {
@@ -14,14 +15,43 @@ namespace EShop.Controllers
 		{
 			return View();
 		}
-		public async Task<IActionResult> Details(long Id)
+		//public async Task<IActionResult> Detail(long Id)
+		//{
+		//	if (Id == null)
+		//	{
+		//		return RedirectToAction("Index");
+		//	}
+		//	var productsById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault();
+		//	return View(productsById);
+		//}
+		public async Task<IActionResult> Detail(long Id)
 		{
 			if (Id == null)
 			{
 				return RedirectToAction("Index");
 			}
-			var productById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault();
-			return View(productById);
+			var productsById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault(); //category = 4
+
+			// related product
+			var relatedProducts = await _dataContext.Products
+				.Where(p => p.CategoryId == productsById.CategoryId && p.Id != productsById.Id)
+				.Take(4)
+				.ToListAsync();
+
+			ViewBag.RelatedProducts = relatedProducts;
+
+			return View(productsById);
+		}
+
+		public async Task<IActionResult> Search(string searchTerm)
+		{
+			var product = await _dataContext.Products
+				.Where(p => p.Name.Contains(searchTerm) || p.Description.Contains(searchTerm))
+				.ToListAsync();
+
+			ViewBag.Keyword = searchTerm;
+
+			return View(product);
 		}
 
 	}
