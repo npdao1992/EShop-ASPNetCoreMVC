05a3255338d0a38fcac5149697332610fa71c52e
npdao1992 <npdao1992@gmail.com>
2024-10-13 23:48:50 +0700

    Update: Filter product categories - Product css details

--- EShop/Controllers/CategoryController.cs
+++ EShop/Controllers/CategoryController.cs
@@ -17,48 +17,91 @@ namespace EShop.Controllers
 
 
 
-        public async Task<IActionResult> Index(string Slug = "")
-        {
-            CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
-            if (category == null)
-            {
-                return RedirectToAction("Index");
-            }
-            var productByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+		public async Task<IActionResult> Index(string Slug = "", string sort_by = "", string startprice = "", string endprice = "")
+		{
+			CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
+			if (category == null)
+			{
+				return RedirectToAction("Index");
+			}
+			// Lấy tất cả sản phẩm
+			IQueryable<ProductModel> productByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+			var count = await productByCategory.CountAsync();
 
-            return View(await productByCategory.OrderByDescending(p => p.Id).ToListAsync());
-        }
+			if (count > 0)
+			{
+				if (sort_by == "price_increase")
+				{
+					productByCategory = productByCategory.OrderBy(p => p.Price);
+				}
+				else if (sort_by == "price_decrease")
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Price);
+				}
+				else if (sort_by == "price_newest")
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Id);
+				}
+				else if (sort_by == "price_oldest")
+				{
+					productByCategory = productByCategory.OrderBy(p => p.Id);
+				}
+				// Lọc giá sản phẩm
+				else if (startprice != "" && endprice != "")
+				{
+					decimal startPriceValue;
+					decimal endPriceValue;
 
-        //public async Task<IActionResult> Index(string Slug = "")
-        //{
-        //    // Lấy thông tin danh mục theo Slug
-        //    var category = _dataContext.Categories
-        //        .Where(c => c.Slug == Slug)
-        //        .FirstOrDefault();
+					if (decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice, out endPriceValue))
+					{
+						productByCategory = productByCategory.Where(p => p.Price >= startPriceValue && p.Price <= endPriceValue);
+					}
+					else
+					{
+						productByCategory = productByCategory.OrderByDescending(p => p.Id);
+					}
+				}
+				else
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Id);
+				}
 
-        //    if (category == null)
-        //    {
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    // Lấy sản phẩm theo danh mục
-        //    var products = _dataContext.Products
-        //        .Where(p => p.CategoryId == category.Id)
-        //        .OrderByDescending(p => p.Id)
-        //        .ToList();
-
-        //    // Tạo đối tượng CategoryViewModel
-        //    var categoryViewModel = new CategoryViewModel
-        //    {
-        //        Category = category,
-        //        Products = products,
-        //        ProductCount = products.Count
-        //    };
-
-        //    // Trả về View với CategoryViewModel
-        //    return View(categoryViewModel);
-        //}
+			}
 
 
-    }
+			return View(await productByCategory.ToListAsync());
+		}
+
+		//public async Task<IActionResult> Index(string Slug = "")
+		//{
+		//    // Lấy thông tin danh mục theo Slug
+		//    var category = _dataContext.Categories
+		//        .Where(c => c.Slug == Slug)
+		//        .FirstOrDefault();
+
+		//    if (category == null)
+		//    {
+		//        return RedirectToAction("Index");
+		//    }
+
+		//    // Lấy sản phẩm theo danh mục
+		//    var products = _dataContext.Products
+		//        .Where(p => p.CategoryId == category.Id)
+		//        .OrderByDescending(p => p.Id)
+		//        .ToList();
+
+		//    // Tạo đối tượng CategoryViewModel
+		//    var categoryViewModel = new CategoryViewModel
+		//    {
+		//        Category = category,
+		//        Products = products,
+		//        ProductCount = products.Count
+		//    };
+
+		//    // Trả về View với CategoryViewModel
+		//    return View(categoryViewModel);
+		//}
+
+
+	}
 }
